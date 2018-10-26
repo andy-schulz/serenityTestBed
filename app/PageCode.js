614a659baaf79554b3495b52133e0aa459cf7a28
Andy Schulz <andy.schulz@test-steps.de>
2018-10-26 07:45:29 +0200

    add html test page code

---
+++ app/PageCode.js
@@ -0,0 +1,23 @@
+function testfunc (cssSelector, searchText, using) {
+    using = using || document;
+
+    if (searchText.indexOf('__REGEXP__') === 0) {
+        var match = searchText.split('__REGEXP__')[1].match(/\/(.*)\/(.*)?/);
+        searchText = new RegExp(match[1], match[2] || '');
+    }
+    var elements = using.querySelectorAll(cssSelector);
+    var matches = [];
+    for (var i = 0; i < elements.length; ++i) {
+        var element = elements[i];
+        var elementText = element.textContent || element.innerText || '';
+        var elementMatches = searchText instanceof RegExp ?
+            searchText.test(elementText) :
+            elementText.indexOf(searchText) > -1;
+
+        if (elementMatches) {
+            matches.push(element);
+        }
+    }
+
+    return matches;
+}
